05d6255383593cc3fe186668259c60bc0345807d
DevArtJob <jbaccdev@gmail.com>
2023-02-25 12:22:53 +0300

    mobile navigation done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg"
+    } else {
+        navBtnImg.src = "./img/icons/nav-open.svg"
+
+    }
+}
